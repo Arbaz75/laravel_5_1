a356dbcae5984a9191a74dd8af6b3b865988e72e
Arbaz Rizvi <arbaz.rizvi@sofmen.com>
2015-09-08 16:33:02 +0530

    Alter sql updates file

--- database/sql/4_add_foreign_key.sql
+++ database/sql/4_add_foreign_key.sql
@@ -1,5 +1,5 @@
-ALTER TABLE interval_records ADD FOREIGN KEY ( event_id ) REFERENCES event( event_id )
+ALTER TABLE interval_records ADD FOREIGN KEY ( event_id ) REFERENCES event( event_id );
 
-ALTER TABLE event ADD FOREIGN KEY ( member_id ) REFERENCES members( member_id )
+ALTER TABLE event ADD FOREIGN KEY ( member_id ) REFERENCES members( member_id );
 
-ALTER TABLE member_token ADD FOREIGN KEY ( member_id ) REFERENCES members( member_id )
+ALTER TABLE member_token ADD FOREIGN KEY ( member_id ) REFERENCES members( member_id );
